78c665c9187b355ad72de725a0664a90e913db86
AnshikaRastogi29 <anshikarastogi0129@gmail.com>
2026-04-28 21:35:18 +0530

    Add files via upload

---
+++ assemblycode_generator.h
@@ -0,0 +1,9 @@
+#ifndef ASSEMBLYCODE_GENERATOR_H
+#define ASSEMBLYCODE_GENERATOR_H
+
+#include <string>
+
+void generateAssembly(const std::string& inputFile,
+                      const std::string& outputFile);
+
+#endif
